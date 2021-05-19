b9b8318e507f6a781fac6a0e6b4dbb5fbb154465
jeck-vorobeq <m.a.karasev2011@gmale.com>
2021-05-19 17:02:32 +0300

    fla cat

--- __mayn__.py
+++ __mayn__.py
@@ -4,11 +4,12 @@ wrap.add_sprite_dir("C:/Users/nnata/wrap_py_catalog")
 speed = 1
 
 # окно
-wrap.world.create_world(1930, 1010)
+wrap.world.create_world(1930, 1010,1200,0)
 shon = wrap.sprite.add("backgrounds", 965, 505)
 wrap.sprite.set_size(shon, 1929, 1009)
 
 # трубы
+pipe3 = wrap.sprite.add("flappy_bird", 712, 500, "pipe")
 pipe = wrap.sprite.add("flappy_bird", 700, 500, "pipe")
 wrap.sprite.set_size(pipe, 85, 370)
 wrap.sprite.move_bottom_to(pipe, 802)
@@ -16,10 +17,7 @@ pipe2 = wrap.sprite.add("flappy_bird", 700, 500, "pipe")
 wrap.sprite.set_reverse_y(pipe2,True)
 wrap.sprite.set_size(pipe2, 85, 370)
 wrap.sprite.move_top_to(pipe2,0)
-
-
-
-
+pipelist=[pipe,pipe2,pipe3]
 
 
 
@@ -38,7 +36,7 @@ wrap.sprite.set_height_proportionally(player, 100)
 @wrap.on_key_down(wrap.K_SPACE)
 def fly():
     global speed
-    speed = -10
+    speed = -20
 
 
 # падение
@@ -54,10 +52,10 @@ def fall():
         os._exit(6)
     a = wrap.sprite.get_angle(player)
     b=a+ speed/2
-    if b <0:
-        b=0
-    if b >180:
-        b=180
+    if b <45:
+        b=45
+    if b >150:
+        b=150
     wrap.sprite.set_angle(player, b )
 
 
@@ -65,12 +63,12 @@ def fall():
 
 @wrap.always(30)
 def dvishenie():
-    if wrap.sprite.is_collide_sprite(pipe,player):
-        os._exit(0)
-    if wrap.sprite.is_collide_sprite(pipe2,player):
-        os._exit(0)
-#     wrap.sprite.move(pipe,-3,0)
-#     wrap.sprite.move(pipe2, -3, 0)
+    for p in pipelist:
+
+        if wrap.sprite.is_collide_sprite(p,player):
+            os._exit(0)
+        wrap.sprite.move(p,-3,0)
+
 
 
 
